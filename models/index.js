ffdb3f3dccf0994c30c27d80ded985c2ad94fe01
Richard Hong <rhong24@gmail.com>
2022-11-01 20:52:27 -0500

    adding model attributes, adding relationship between models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: ProductTag
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {
+  through: ProductTag
+})
 
 module.exports = {
   Product,
