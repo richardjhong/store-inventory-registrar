0fd1d86960a0dac05990051d5d3bbca9ae5ffe28
Richard Hong <rhong24@gmail.com>
2022-11-02 20:36:56 -0500

    modifying localhost to 127.0.0.1 to connect to sql, looking into problem later, adding many to many relations within models/index.js, modifying tag_id within Product to not be unique, building routes for category and product

--- models/index.js
+++ models/index.js
@@ -14,12 +14,18 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE'
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tags, {
-  through: ProductTag
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Products, {
-  through: ProductTag
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
 })
 
 module.exports = {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    })
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -46,6 +60,30 @@ router.post('/', (req, res) => {
       res.status(400).json(err);
     });
 });
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const productData = await Product.create(req.body)
+//         // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+//     if (req.body.tagIds.length) {
+//       const productTagIdArr = req.body.tagIds.map((tag_id) => {
+//         return {
+//           product_id: product.id,
+//           tag_id,
+//         };
+//       });
+//       return ProductTag.bulkCreate(productTagIdArr);
+//     }
+//     res.status(200).json(productData);
+//     res.status(200).json(productTagIds)
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).json(err);
+//   }
+// });
+
+
+
 
 // update product
 router.put('/:id', (req, res) => {
